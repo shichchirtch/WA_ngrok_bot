8fa7b3aa6d3a398a6a6ee5f8214c541e1cc93738
shichchirtch <ivanvol9l@gmail.com>
2025-03-04 18:28:00 +0100

    3 Commit
    Код приведен в соотвтествие  с первой версией бота

--- static/script.js
+++ static/script.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let pricePizzaElement = document.querySelector("#price");
         let basePrice = parseFloat(pricePizzaElement.getAttribute("data-price")); // Берём цену из HTML
         let quantity = 1;
-        const BASE_URL = "https://4437-2a00-20-8-1dfb-c3-5496-c60c-67b1.ngrok-free.app";
+        const BASE_URL = "https://720a-2a00-20-8-1dfb-c3-5496-c60c-67b1.ngrok-free.app";
 
         decreaseBtn.addEventListener("click", function () {
             if (quantity > 1) {
